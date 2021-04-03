1476f0fb3997eee09e9acf2e6b69a8a3f6141abb
Andy77q <andreybuh77@gmail.com>
2021-04-03 13:55:06 +0300

    sozdal pole i ti vari hodov

--- first.py
+++ first.py
@@ -1,13 +1,21 @@
 #название игры в терминале
 pres = """ 
-           x    x   ooo       ggg      aa      m       m     eee
-            x  x   o   o     g        a  a     m m   m m    e   
-             xx    o   o     g  gg   aaaaaa    m  m m  m    eeeee
-            x  x   o   o     g   g   aaaaaa    m   m   m    e   
-           x    x   ooo       ggg    a    a    m       m     eee   
-                                                                   """
+           x    x   ooo        ggg      aa      m       m     eee
+            x  x   o   o      g        a  a     m m   m m    e   
+             xx    o   o      g  gg   aaaaaa    m  m m  m    eeeee
+            x  x   o   o      g   g   aaaaaa    m   m   m    e   
+           x    x   ooo        ggg    a    a    m       m     eee   
+                                                                    """
 
 print(pres)
+#Приглашаем пользователя  погирать
+print("Lets play in XXX & OOO")
+#импортирую данные из второго
+import second.py as sc
+#beg = sc.empty
+print(beg)
+
+
 
 
 
